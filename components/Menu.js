3b8b6bcc69a10ed16e419c48d3188ad221ec532e
dragon script <agomila96@gmail.com>
2021-01-30 19:58:21 +0000

    Add menu

---
+++ components/Menu.js
@@ -0,0 +1,7 @@
+import styles from '../styles/Menu.module.css'
+
+const Menu = () => <div className={styles.menu}>
+dfsds
+</div>
+
+export default Menu
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import styles from '../styles/App.module.css'
+import Menu from '../components/Menu'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className={styles.app}>
+      <Menu />
+      <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
